4484d7205d31eee42ca3b9ac132cdf7def0e7fcb
MugeSo <muges@mugeso.com>
2014-04-22 14:35:13 +0900

    Cleanup resources.

--- src/main/java/jp/co/webnium/yabumi/app/ViewerActivity.java
+++ src/main/java/jp/co/webnium/yabumi/app/ViewerActivity.java
@@ -129,7 +129,7 @@ public class ViewerActivity extends Activity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.share_button).setOnTouchListener(mDelayHideTouchListener);
 
         handleIntent(getIntent());
     }
